27a14f720dc8f13cb80e67a7ed902e63865fd58a
WilliamBerlin76 <william.berlin76@gmail.com>
2020-03-30 18:49:31 -0500

    testing deploy

--- knexfile.js
+++ knexfile.js
@@ -36,10 +36,10 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
-    pool: {
-      min: 2,
-      max: 10
-    },
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
     migrations: {
       directory: './database/migrations'
     },
